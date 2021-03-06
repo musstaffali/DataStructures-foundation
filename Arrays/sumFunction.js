26cf960757c80f67c9d3798dbe074a7fbd9c655e
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-03-06 17:57:12 -0500

    sumFunction

---
+++ Arrays/sumFunction.js
@@ -0,0 +1,16 @@
+// sumFunction array method
+const items = [
+    { name: 'Bike',   price: 100 },
+    { name: 'TV',   price: 200  },
+    { name: 'Album',   price: 10  },
+    { name: 'Book',   price: 5  },
+    { name: 'Phone',   price: 500  },
+    { name: 'Computer',   price: 1000  },
+    { name: 'Keyboard',   price: 25   }
+];
+// function to check true or false on every items price 
+const hasInexpensiveItems = items.every((item) => {
+    return item.price <= 100
+})
+
+console.log(hasInexpensiveItems)
