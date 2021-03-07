4fc66910ecfc007a267561fcd8b1233420f29b14
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-03-06 19:21:20 -0500

    8 method arrays

--- Arrays/reduce.js
+++ Arrays/reduce.js
@@ -1,5 +1,5 @@
 
-// sumFunction array method
+// reduce method array 
 const items = [
     { name: 'Bike',   price: 100 },
     { name: 'TV',   price: 200  },
@@ -9,9 +9,9 @@ const items = [
     { name: 'Computer',   price: 1000  },
     { name: 'Keyboard',   price: 25   }
 ];
-// function to check true or false on every items price 
-const hasInexpensiveItems = items.every((item) => {
-    return item.price <= 100
-})
+// function to sum all items starting from 0
+const total = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal
+}, 0)
 
-console.log(hasInexpensiveItems)
+console.log(total)
