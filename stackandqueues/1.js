dbbcb3500ab0c352fa0b62f3929e79feb2e388be
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-13 14:09:21 -0400

    pushpop

--- stackandqueues/1.js
+++ stackandqueues/1.js
@@ -4,10 +4,6 @@ Three In One:
 Describe how you could use a single array
     to implement three stacks.
 
-Iterate through linked list getting one node at a time
-Iterate through the rest of the linked list and compare the current node’s value
-When duplicate element is found, remove it from the linked list and update the linked list.
-
 
 Hint
 #2 
@@ -17,6 +13,23 @@ Can you simulate a single stack using an array?
 Remember that there are many possible solutions, and
 there are tradeoffs of each.
 
+#12
+We could simulate three stacks in an array by just 
+allocating the first third of the array to the
+first stack.
+One might actually be much bigger than the others, thought.
+Can we be more flexible with the division.
+
+#38
+If you want to allow for flexible  divisions, 
+you can shift stacks around.
+Can you ensure that all availble capacity is used?
+
+#58
+Try thinking about the arrat as circular, such that the end
+of the array "wraps around" to the start of thw array.
+
+
 Input:
 Output: 
 Constraints: 
@@ -25,3 +38,36 @@ Edge cases:
 time complexity: linear 0(N^2)
 space complexity: 
  */
+
+var ThreeInOne = function() {
+    this.container = [];
+    this.middleBottom = 0;
+    this.middleTop = 0;
+};
+
+ThreeInOne.prototype.push1 = function(value) {
+    this.container.unshift(value);
+    this.middleBottom++;
+    this.middleTop++;
+};
+
+ThreeInOne.prototype.push2 = function(value) {
+    this.container.splice(this.middleTop, 0, value);
+    this.middleTop++;
+};
+
+ThreeInOne.prototype.push3 = function(value) {
+    this.container.push(value);
+};
+
+ThreeInOne.prototype.pop1 = function() {
+    if (this.isEmpty1()) {
+        return undefined;
+    }
+    var answer = this.container.shift();
+    if (this.middleBottom > 0) {
+        this.middleBottom--;
+        this.middleTop--;
+    }
+    return answer;
+};
