844e36b648776f72010e6baca50b9d95e4340b35
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-13 12:25:44 -0400

    stack .1

---
+++ stackandqueues/1.js
@@ -0,0 +1,19 @@
+/**
+CTCI - 3.1
+Three In One:
+Describe how you could use a single array
+    to implement three stacks.
+
+Iterate through linked list getting one node at a time
+Iterate through the rest of the linked list and compare the current node’s value
+When duplicate element is found, remove it from the linked list and update the linked list.
+
+
+Input:
+Output: 
+Constraints: 
+Edge cases: 
+
+time complexity: linear 0(N^2)
+space complexity: 
+ */
