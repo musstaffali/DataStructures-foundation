327339a238c26f78d9321fc9cb7fa397ce7b2f65
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-14 19:49:12 -0400

    kate

--- Trees&Graphs/1.js
+++ Trees&Graphs/1.js
@@ -17,53 +17,62 @@ Constraints: Optimize
 Edge cases: empty strinf, spaces between and in front and end, more than 2 of the same char, even and odd char
  */
 
-class QueueNode {
-
-    constructor( data ) {
-        this.data = data
-        this.next = null
+class Node {
+    constructor(val){
+      this.val = val;
+      this.children = [];
     }
-
-}
-
-class Queue {
-
-    constructor() {
-        this.first = null
-        this.last = null
+  }
+  
+  let Rob = new Node('Rob')
+  let kate = new Node("Kate")
+  let dave = new Node('Dave')
+  let dan = new Node("dan")
+  Rob.children = [dave, kate, dan]
+  let dog = new Node("dog")
+  kate.children.push(dog)
+  
+  class LinkedList{
+    constructor(){
+      this.head = null;
     }
-
-    add( item ) {
-        const newNode = new QueueNode( item )
-        if ( this.last ) {
-            this.last.next = newNode
-        } else {
-            this.first = newNode
+    add(node){
+      let currentHead = this.head
+      if (!currentHead) this.head = node;
+      else {
+        while(currentHead.next) {
+          currentHead = currentHead.next
         }
-        this.last = newNode
+        currentHead.next = node;
+      }
     }
-
-    remove() {
-        if ( this.first ) {
-            const data = this.first.data
-            this.first = this.first.next
-            if ( this.first == null ) {
-                this.last == null
-            }
-            return data
-        }
-        throw Error( 'empty queue' )
+    popHead(){
+      let temp = this.head;
+      this.head = this.head.next
+      return temp;
     }
-
-    peek() {
-        if ( this.first ) {
-            return this.first.data
-        }
-        throw Error( 'empty queue' )
+  }
+  
+  class LLNode{
+    constructor(val){
+      this.val = val;
+      this.next = null;
     }
-
-    isEmpty() {
-        return this.first === null 
+  }
+  
+  //Breadth-First Search
+  function searchNode(root, destination){
+    let queue = new LinkedList();
+    queue.add(new LLNode(root))
+    while (queue.head){
+      root.children.forEach(child => {
+        queue.add(new LLNode(child))
+      })
+      let temp = queue.popHead();
+      if (temp.val===destination) return true;
+      else root.val = temp  
     }
-
-}
+    return false;
+  }
+  
+  searchNode(kate, dog)
